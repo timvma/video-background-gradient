cd85fefee82f091be1bde15c17c6e8bf0d721051
tim <timawbi@gmail.com>
2024-04-15 13:55:26 +0900

    stagger fix

--- app.js
+++ app.js
@@ -2,18 +2,22 @@ let videoController = document.querySelectorAll('.video__controller')
     
 videoController.forEach(c=> {
     let bg = c.nextElementSibling
-    console.log(bg)
-    
     c.addEventListener('play', ()=> {
-        
         bg.play()
         if(!bg.classList.contains('video__controller--movie'))
             bg.classList.add('video__controller--movie')
+        
+            bg.currentTime = c.currentTime
+
+
     })
+
     c.addEventListener('pause', ()=> {
         bg.pause()
-    })
-    c.addEventListener('timeupdate', function() {
         bg.currentTime = c.currentTime
+    })
+
+    c.addEventListener('timeupdate', function() {
+        //bg.currentTime = c.currentTime
     });
 })
